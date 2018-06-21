56928d28d742b63fe9e707f7d0b3a99d1ae7ec14
Anatoly Griko <anhel@ngs.ru>
2018-06-21 21:10:27 +0700

    Начата работа над приложением Info_pages

--- eshop/info_pages/views.py
+++ eshop/info_pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template.exceptions import TemplateDoesNotExist
+from django.http import Http404
 
 # Create your views here.
+def page_view(request, page_name):
+    # 1. Попытаться вывести шаблон page_name.html
+    # 2. Если его нет, сообщить, что страницы нет
